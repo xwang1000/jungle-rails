c9063f15e00a4138d1caf5dd61463ee39f6d4a13
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-03-25 06:39:18 +0000

    Finish review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    raise "Yo I'm here!"
+    review_input = params[:review]
+    @review = Review.new
+    @review.product_id = params[:product_id]
+    @review.rating = review_input[:rating]
+    @review.description = review_input[:description]
+    @review.user_id = current_user.id
+
+    @review.save
+
+    if @review.save
+      redirect_to "/products/" + params[:product_id], notice: 'Review created!'
+    else
+      render "/products/" + params[:product_id]
+    end
   end
 end
